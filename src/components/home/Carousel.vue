442bf8bfa1a3cf4eaf14b0dd3ff3b703a2412004
Jonathan Sax <jsax8310@outlook.com>
2017-10-31 14:27:42 -0600

    fixed image with misspelling

--- src/components/home/Carousel.vue
+++ src/components/home/Carousel.vue
@@ -7,7 +7,7 @@
       </ol>
       <div class="carousel-inner">
         <div class="carousel-item active">
-          <img class="d-block w-100" src="../../assets/BCIVICPOC1stSlide.png" alt="First slide">
+          <img class="d-block w-100" src="../../assets/BCIVICPOCslide1.png" alt="First slide">
         </div>
         <div class="carousel-item">
           <img class="d-block w-100" src="../../assets/BCIVICPOCslide2.png" alt="Second slide">
